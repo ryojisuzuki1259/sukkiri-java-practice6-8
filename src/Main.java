2f4c490fcdfd7822223b5d8ba94c26d27e238f8d
suzukiryoji <suzukiryoji@localhost>
2021-04-15 02:25:59 +0900

    Write codes

--- src/Main.java
+++ src/Main.java
@@ -8,12 +8,5 @@ public class Main {
 		showMondokoro();
 	}
 
-	public static void callDeae() {
-		System.out.println("えぇい、こしゃくな。くせ者だ！であえい！");
-	}
-	public static void showMondokoro() throws Exception{
-		System.out.println("飛車さん、角さん。もういいでしょう。");
-		System.out.println("この紋所が目に入らぬか！");
-		doTogame();
-	}
+
 }
